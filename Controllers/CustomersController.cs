8e837b8e8cac3b908a2034894e0e0f2be06203e3
YenXm <moonvoyager2@protonmail.com>
2021-12-13 15:48:56 -0500

    Added New Customer Endpoint for login verif

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -3,29 +3,45 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using RocketApi.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RocketApi.Models;
 
 namespace RocketApi.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class CustomersController : ControllerBase
-  {
-    private readonly ApplicationContext _context;
-
-    public CustomersController(ApplicationContext context)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
     {
-      _context = context;
+        private readonly ApplicationContext _context;
+
+        public CustomersController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        //-------------------------------------------------- Get all Customers ----------------------------------------------------\\
+
+        //Get: api/Customers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _context.customers.ToListAsync();
+        }
+
+        [HttpGet("{customer_email}")]
+        // Email of the customer that we want to verify existence of
+        public async Task<bool> GetIfExist(string customerEmail)
+        {
+            // Verify if a customer with that email exists
+            return _context.customers.Any(e => e.email_of_the_company_contact == customerEmail);
+        }
+
+
     }
-
-    //-------------------------------------------------- Get all Customers ----------------------------------------------------\\
-    
-    //Get: api/Customers
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
-    {
-      return await _context.customers.ToListAsync();
-    }
-
-
-  }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,14 @@
 // Unused usings removed
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using RocketApi.Models;
 using Npgsql;
-//using RocketApi.Models;
+using RocketApi.Models;
 
+//using RocketApi.Models;
 namespace RocketApi
 {
     public class Startup
@@ -23,14 +23,18 @@ namespace RocketApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            var connectionString = Configuration.GetConnectionString("WebApiDatabase"); // MYSQL_CONNECTION_STRING WebApiDatabase
-            var psqlConnectionString = Configuration.GetConnectionString("PostGresqlDatabase");
-            var serverVersion = new MySqlServerVersion(new System.Version(8, 0, 27));
-            services.AddDbContext<ApplicationContext>(opt =>
-                                               opt.UseMySql(connectionString, serverVersion));
-            services.AddDbContext<PostGresqlContext>(opt =>
-                                                opt.UseNpgsql(psqlConnectionString));
-
+            var connectionString =
+                Configuration.GetConnectionString("WebApiDatabase"); // MYSQL_CONNECTION_STRING WebApiDatabase
+            var psqlConnectionString =
+                Configuration.GetConnectionString("PostGresqlDatabase");
+            var serverVersion =
+                new MySqlServerVersion(new System.Version(8, 0, 27));
+            services
+                .AddDbContext<ApplicationContext>(opt =>
+                    opt.UseMySql(connectionString, serverVersion));
+            services
+                .AddDbContext<PostGresqlContext>(opt =>
+                    opt.UseNpgsql(psqlConnectionString));
 
             // services.AddDbContext<ElevatorsContext>(opt =>
             //                                   opt.UseInMemoryDatabase("relational_database"));
@@ -54,10 +58,11 @@ namespace RocketApi
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
         }
     }
 }
